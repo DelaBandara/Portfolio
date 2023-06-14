1131f10defae1a2a70a6e7c955e431045945395a
DelaBandara <120809445+DelaBandara@users.noreply.github.com>
2023-06-14 13:10:32 +0530

    Initial commit

---
+++ PageTransitions.js
@@ -0,0 +1,4 @@
+function PageTransitions() {
+
+    PageTransitions();
+}
---
+++ app.js
@@ -0,0 +1,18 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controlls');
+const sectBtn = document.querySelectorAll('.control');
+const allsections = document.querySelector('.main-content');
+
+
+function PageTransitions(){
+      //button click active class
+      for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn','');
+            this.className += ' active-btn';
+        })
+    }
+}
+
+PageTransitions();
